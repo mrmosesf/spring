4d91ea2b3377b7587db79054322020988df9abe8
Moses Franco <mosesf@outlook.com>
2017-06-19 16:08:55 -0500

    Completed logic for the dice roll exercise

--- src/main/java/com/codeup/controllers/HomeController.java
+++ src/main/java/com/codeup/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by Moses Franco on 6/19/17
@@ -13,17 +15,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String home(){
+	public String home() {
 		return "index";
 	}
 
 	@GetMapping("/portfolio")
-	public String portfolio(){
+	public String portfolio() {
 		return "portfolio";
 	}
 
 	@GetMapping("/resume")
-	public String resume(){
+	public String resume() {
 		return "resume";
+	}
+
+	@GetMapping("/roll-dice")
+	public String guessANumber(Model model) {
+		model.addAttribute("message", "Pick a number, roll the dice!");
+		return "roll-dice";
+	}
+
+	@GetMapping("/roll-dice/{guess}")
+	public String pickedNumber(@PathVariable int guess, Model model) {
+		int randomNumber = (int) Math.floor(Math.random() * (6 - 1 + 1) + 1);
+
+		model.addAttribute("number", guess);
+		model.addAttribute("random", randomNumber);
+
+		if (guess == randomNumber) {
+			model.addAttribute("message", "You guessed correctly!");
+		} else {
+			model.addAttribute("message", "Guess again!");
+		}
+		return "roll-dice";
 	}
 }
