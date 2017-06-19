2cef2866ff816db9ade9580ec0a61a9619031f40
Moses Franco <mosesf@outlook.com>
2017-06-19 15:01:02 -0500

    Controller to handle new exercises for `resume` and `portfolio`

---
+++ src/main/java/com/codeup/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Moses Franco on 6/19/17
+ * Codeup
+ * Pinnacles
+ */
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home(){
+		return "index";
+	}
+
+	@GetMapping("/portfolio")
+	public String portfolio(){
+		return "portfolio";
+	}
+
+	@GetMapping("/resume")
+	public String resume(){
+		return "resume";
+	}
+}
