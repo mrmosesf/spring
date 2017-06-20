362c8fc85d28c6e9a2f8c6ac5888ee8fb32b7a60
Moses Franco <mosesf@outlook.com>
2017-06-20 13:54:16 -0500

    Created a call for `Post` that describes all the functions of a single post

---
+++ src/main/java/com/codeup/models/Post.java
@@ -0,0 +1,41 @@
+package com.codeup.models;
+
+/**
+ * Created by Moses Franco on 6/20/17
+ * Codeup
+ * Pinnacles
+ */
+public class Post {
+	private long id;
+	private String title;
+	private String text;
+
+	public Post(String title, String text) {
+		this.title = title;
+		this.text = text;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
