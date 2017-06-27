864e40dcec7f0c02dde0a8d77d276e0ec3d1ef1c
Moses Franco <mosesf@outlook.com>
2017-06-27 09:42:24 -0500

    `PostMapping` controller after a use has registered

---
+++ src/main/java/com/codeup/controllers/UsersController.java
@@ -0,0 +1,34 @@
+package com.codeup.controllers;
+
+import com.codeup.models.User;
+import com.codeup.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+/**
+ * Created by mosesfranco on 6/26/17
+ * Codeup
+ * Pinnacles
+ */
+@Controller
+public class UsersController {
+	@Autowired
+	UsersRepository usersDao;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+
+	@PostMapping("/users/register")
+	public String saveUser(@Valid User user, Errors validation){
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		usersDao.save(user);
+
+		return "redirect:/posts";
+	}
+}
